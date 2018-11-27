313dff7446c9c4940e2c0209a27f3201fb4305d9
Unknown <xjulien1122@gmail.com>
2018-11-27 17:36:39 -0500

    Updated path to model

--- molopt_optimize.sh
+++ molopt_optimize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python molopt/optimize.py --test data/opt.test.logP-SA --vocab data/vocab.txt --hidden 420 --depth 3 --latent 56 --sim 0.2 --model joint-h420-L56-d3-beta0.005/model.iter-4
+python molopt/optimize.py --test data/opt.test.logP-SA --vocab data/vocab.txt --hidden 420 --depth 3 --latent 56 --sim 0.2 --model molopt/joint-h420-L56-d3-beta0.005/model.iter-4
